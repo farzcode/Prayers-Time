cce460c06f5caba88329aef1a43d533dc4a67bef
farzcode <faridkurniawan204@gmail.com>
2021-06-09 14:11:12 +0700

    Api Aladhan

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-function succes(position){
-    console.log(position);
+function prayerTimes(latitude, longitude){
+    fetch('http://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=2')
+    .then(response => response.json())
+    .then(function(response){
+        console.log(response.data[0]);
+    });
+}
+
+function success(position){
+    prayerTimes(position.coords.latitude, position.coords.longitude);
 }
 
 function error(){
@@ -10,7 +18,7 @@ function userLocation(){
     if(!navigator.geolocation){
         alert('Geolocation tidak didukung didalam browser anda, silahkan gunakan browser lain');
     }else{
-        navigator.geolocation.getCurrentPosition(succes, error);
+        navigator.geolocation.getCurrentPosition(success, error);
     }
 }
 
